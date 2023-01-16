0d365ca715ad52cbeb0d7ac44ead9bd4db227689
Nick Chan <nickncchan@gmail.com>
2023-01-15 19:26:16 -0800

    Minor updates

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { useState } from "react";
 
 export default function Home() {
   const router = useRouter();
